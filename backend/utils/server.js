fde261b1dfb0a0a1dfdfc1c75fc879d99da72a17
milo2112 <mjrp.ros@gmail.com>
2023-10-07 20:57:49 -0300

    adding server listener, default route

--- backend/utils/server.js
+++ backend/utils/server.js
@@ -1,2 +1,16 @@
 require('dotenv').config()
-const fs = require('fs')
+// const fs = require('fs')
+
+const express = require('express')
+
+const PORT = process.env.PORT ?? 3000
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/posts', (_, res) => res.status(200).console.log('hola mundo!'))
+
+app.all('*', (_, res) => res.status(404).json({ code: 404, message: 'Page Not Found' }))
+
+app.listen(PORT, () => console.log(`Server started on: http://localhost:${PORT}`))

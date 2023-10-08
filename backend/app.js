cc7097a86879d191d944798cc44eec2ec743fe40
milo2112 <mjrp.ros@gmail.com>
2023-10-08 11:49:56 -0300

    implementing posts route in server

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,1 @@
-require('./utils/server')
+require('./server')
--- backend/server/index.js
+++ backend/server/index.js
@@ -1,15 +1,17 @@
 require('dotenv').config()
+const cors = require('cors')
 // const fs = require('fs')
-
 const express = require('express')
 
 const PORT = process.env.PORT ?? 3000
-
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
-app.get('/posts', (_, res) => res.status(200).console.log('hola mundo!'))
+app.get('/posts', (_, res) => {
+  res.json([])
+})
 
 app.all('*', (_, res) => res.status(404).json({ code: 404, message: 'Page Not Found' }))
 

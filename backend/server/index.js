5b7feb4729f30c285957bd8e1dbbb2775c04a210
milo2112 <mjrp.ros@gmail.com>
2023-10-14 15:42:41 -0300

    adding update feature for likes

--- backend/server/index.js
+++ backend/server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const cors = require('cors')
 const express = require('express')
-const { readPosts, createPosts } = require('../utils/pg')
+const { readPosts, createPosts, updateLikes } = require('../utils/pg')
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
@@ -17,6 +17,17 @@ app.post('/posts', async (req, res) => {
   const { titulo, url, descripcion } = req.body
   const posts = await createPosts({ titulo, url, descripcion })
   res.status(201).json(posts)
+})
+
+app.put('/posts/like/:id', async (req, res) => {
+  // const { id } = req.params
+  // const { likes } = req.body
+  // console.log(`así entran al server ${id}, ${likes}`)
+  // console.log(typeof likes)
+  // console.log(Number(likes))
+  updateLikes(req.params.id)
+    .then((dbResponse) => res.status(dbResponse?.code ? 500 : 200).json(dbResponse))
+    .catch((error) => res.status(500).json(error))
 })
 
 app.all('*', (_, res) => res.status(404).json({ code: 404, message: 'Page Not Found' }))
